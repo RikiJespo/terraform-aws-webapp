36ccf26f434d7e182f9e377a60d5c2e53ecb2c40
RikiJespo <rikijespo1@gmail.com>
2026-07-29 15:42:39 +0700

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,11 +1,8 @@
-# network.tf
-
 # 1. Membuat VPC
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
-
   tags = {
     Name = "riki-terraform-vpc"
   }
@@ -14,7 +11,6 @@ resource "aws_vpc" "main_vpc" {
 # 2. Membuat Internet Gateway
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main_vpc.id
-
   tags = {
     Name = "riki-terraform-igw"
   }
@@ -26,7 +22,6 @@ resource "aws_subnet" "public_subnet_1" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-southeast-1a"
   map_public_ip_on_launch = true
-
   tags = {
     Name = "riki-public-subnet-1a"
   }
@@ -50,4 +45,38 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route_table_association" "public_rta" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+# 6. Membuat Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "riki-web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "riki-web-sg"
+  }
 }
