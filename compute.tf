6ef59fbf6da83d3df5e7ed2465456abf2de0d486
riki <rikijespo1@gmail.com>
2026-07-29 13:59:18 +0700

    feat: add Ubuntu EC2 instance compute resource

---
+++ compute.tf
@@ -0,0 +1,21 @@
+# Mengambil data sistem operasi Ubuntu terbaru secara otomatis
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # ID Resmi Canonical (Pembuat Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+# Membangun Server Virtual (EC2)
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro" # Spesifikasi ini masuk dalam kategori Gratis (Free Tier)
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "riki-web-server"
+  }
+}
